79a4b252deee7eec08f67238c0503b02509d045a
Vamshi Addanki <vamshi.a4u@gmail.com>
2021-12-21 01:13:14 +0530

    jwt

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express")
+const jwt=require("jsonwebtoken")
+const dotenv=require("dotenv").config()
+
+const app=express()
+const port=4000
+
+app.use(express.json())
+
+
+const key=process.env.KEY
+
+app.get("/",(req,res)=>{
+    res.send("hi")
+})
+
+const verify=(req,res,next)=>{
+    if(Object.keys(req.body).includes("token")){
+        var token=req.body.token
+        try{
+            var user=jwt.verify(token,key)
+            req.body={...req.body,...user}
+            next()
+        }catch(err){
+            res.send({"msg":"Not found"})
+        }
+    }
+    else{
+    res.send({"msg":"error"})
+    }
+}
+
+app.get("/access",verify,(req,res)=>{
+    res.send(`welcome ${req.body.name}`)
+})
+
+
+app.get("/login",(req,res)=>{
+    var user=req.body.user
+    var pass=req.body.pass
+    var name=req.body.name
+    const token =jwt.sign({"user":user,"pass":pass,"name":name},key)
+    res.send(token)
+})
+
+app.listen(port,()=>{
+    console.log(`Listening at ${port}`)
+})
